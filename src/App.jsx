1a0d868a5bbcf500a0908810e3382828cb70ef22
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-08-22 23:37:56 +0200

    implement recipe search onsubmit and render result
    Created basic form for recipe search. useEffect fetches data from the Spoonacular API when query changes. The API request fetches only vegetarian recipes. Added useState to manage the recipe state and the search query. Show the result in Recipe component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Recipe from './components/Recipe'
 import './App.css'
 
-function App() {
+export default function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("apple")
 
+  const apiKey = import.meta.env.VITE_API_KEY 
+  const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&diet=vegetarian&apiKey=${apiKey}`
+
+  useEffect(()=>{
+    getRecipes()
+  }, [query])
+
+  const getRecipes = async() => {
+    const response = await fetch(url)
+    const data = await response.json();
+    setRecipes(data.results)
+    console.log(recipes)
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search)
+    setSearch("")
+  }
 
   return (
     <>
-      <h1>hello react!</h1>
+      <header>
+        <nav>
+          <form className='search-form' onSubmit={getSearch}>
+            <input 
+              className='search-input' 
+              type="text" 
+              placeholder='ingredients' 
+              value={search}
+              onChange={e => setSearch(e.target.value)}
+            />
+            <button className='search-btn' type='submit'>Search</button>
+          </form>
+        </nav>
+      </header>
+
+      <main>
+        {recipes.map(recipe =>(
+          <Recipe key={recipe.id} title={recipe.title} img={recipe.image}/>
+        ))}
+      </main>
     </>
   )
 }
 
-export default App
+
