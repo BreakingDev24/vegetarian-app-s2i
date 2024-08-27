10506921d6db6e2f0beef911125afa06c6754726
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-08-27 17:27:05 +0200

    feat: integrated routing and multiple components into the App
    - Added BrowserRouter, Routes, and Route components from react-router-dom for navigation.
    - Integrated Navbar, SearchForm, Home, RecipeDetails, and SearchResults components.
    - Refactored App to handle search queries and display search results or recipe details based on routes.
    - Included `addRecipeInformation` in API request to fetch more detailed recipe information.

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,45 @@
 import { useState, useEffect } from "react";
 import Recipe from "./components/Recipe";
+import Navbar from "./components/Navbar";
+import SearchForm from "./components/SearchForm";
+import Home from "./components/Home";
+import RecipeDetails from "./components/RecipeDetails";
+import SearchResults from "./components/SearchResults";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 export default function App() {
   const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("apple");
+  const [query, setQuery] = useState("");
 
   const apiKey = import.meta.env.VITE_API_KEY;
-  const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&diet=vegetarian&apiKey=${apiKey}`;
-
+  
   useEffect(() => {
     getRecipes();
   }, [query]);
-
+  
   const getRecipes = async () => {
+    const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&diet=vegetarian&addRecipeInformation=true&apiKey=${apiKey}`;
     const response = await fetch(url);
     const data = await response.json();
     setRecipes(data.results);
     console.log(recipes);
   };
 
-  const getSearch = (e) => {
-    e.preventDefault();
-    setQuery(search);
-    setSearch("");
-  };
 
   return (
     <>
-      <header>
-        <nav>
-          <form className="search-form" onSubmit={getSearch}>
-            <input
-              className="search-input"
-              type="text"
-              placeholder="ingredients"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <button className="search-btn" type="submit">
-              Search
-            </button>
-          </form>
-        </nav>
-      </header>
 
-      <main>
-        {recipes.map((recipe) => (
-          <Recipe key={recipe.id} title={recipe.title} img={recipe.image} />
-        ))}
-      </main>
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <SearchForm setQuery={setQuery}></SearchForm>
+      <Routes>
+        <Route path="/" element={<Home items={recipes}/>}></Route>
+        <Route path="/recipe/:id" element={<RecipeDetails />}></Route>
+        <Route path="/search/:query" element={<SearchResults getRecipes={getRecipes} recipes={recipes}/>}></Route>
+      </Routes>
+
+      </BrowserRouter>
     </>
   );
 }
