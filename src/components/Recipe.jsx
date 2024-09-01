0adba825deea59d3462621a6e7f7a94d2f841835
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-01 10:50:36 +0200

    feat: enhanced Recipe component UI
    - Added readyInMinutes information to the Recipe card for better user experience.
    - Improved layout by structuring content into a recipe card with an image and details section.
    - Included a clock icon next to the cooking time for visual clarity.

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -2,14 +2,17 @@ import {Link} from "react-router-dom"
 
 
 export default function Recipe ({item}) {
-    const {title, image, id, summary} = item
+    const {title, image, id, readyInMinutes} = item
         return(
             <Link to={`/recipe/${id}`}>
                     
-                <div>
-                    <h2>{title}</h2>
+                <div className="recipe-card">
                     <img src={image} alt={title} />
-                    <button>Details</button>
+                    <div className="recipe-info">
+                        <h2>{title}</h2>
+                        <p><span><i className="bi bi-clock"></i></span> {readyInMinutes} min</p>
+                        <button>Details</button>
+                    </div>
                 </div>
             </Link>
         )
