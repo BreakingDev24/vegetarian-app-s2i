2a1880a6c9e53b088468919e7b0ef7e9cecfeb35
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-02 18:46:54 +0200

    Refactored RecipeDetails component and added CSS module
    -Updated component to use CSS modules for styling.
    -Included new CSS class names to match the styles defined in the CSS module.
    -Enhanced the display of recipe details, including title, description, image, ingredients, and instructions.
    - Added fallback loading text for better user feedback during data fetching.

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-
+import DetailCSS from './RecipeDetails.module.css'
 
 export default function RecipeDetails(){
     const {id} = useParams();
@@ -19,17 +19,42 @@ export default function RecipeDetails(){
         fetchRecipeDetails()
     }, [id])
 
+    if(!recipe) return <p>Loading...</p>
+
+    const {title, summary, image, extendedIngredients, analyzedInstructions} = recipe
+
     return(
         <>
-            {recipe ? (
+           
+            <main>
+                <h2 className={DetailCSS['recipe-title']}>{title}</h2>
+                <p dangerouslySetInnerHTML={{__html: summary}} className={DetailCSS['recipe-description']}></p>
+                <div className={DetailCSS['details-container']}>
+                    <div className={DetailCSS['recipe-img-container']}>
+                        <img className={DetailCSS['recipe-img']} src={image} alt={title} />
+                    </div>
+                    <div className={DetailCSS['ingredients-container']}>
+                        <h3>Ingredients</h3>
+                        <ul className={DetailCSS['ingredients-list']}>
+                            {
+                                extendedIngredients.map(({original}, index) => {
+                                    return <li key={index} className={DetailCSS.ingredient}>{original}</li>
+                                    
+                                })
+                            }
+                        </ul>
+                    </div>
+                </div>
 
-                <div>
-                <h2>{recipe.title}</h2>
-                <img src={recipe.image} alt={recipe.title} />
-                <p dangerouslySetInnerHTML={{__html: recipe.summary}}></p>
-            </div>
-            ) : (<p>Loading</p>
-            )}
-        </>
+                <div className={DetailCSS['istructions-container']}>
+                    <h3>Instructions</h3>
+                    <ol className={DetailCSS['istructions-list']}>
+                    {analyzedInstructions[0]?.steps.map(({ step }, index) => (
+                <li key={index}>{step}</li>
+            ))}
+                    </ol>
+                </div>
+            </main>
+    </>
     )
 }
