e72d2b58eab00eebd9954b2f682edd69a86a9575
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-02 18:41:20 +0200

    style: refactored Recipe component with CSS Modules
    - Updated `Recipe` component to use CSS Modules for styling (`Recipe.module.css`).
    - Improved styling of the `Recipe` card including responsive design and better layout.
    - Refactored `recipe-card`, `recipe-info`, `recipe-title`, `recipe-time`, and `show-details` styles.
    - Replaced inline styles with class names from `Recipe.module.css`.

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,20 +1,20 @@
-import {Link} from "react-router-dom"
-
+import {Link} from "react-router-dom";
+import RecipeCSS from './Recipe.module.css'
 
 export default function Recipe ({item}) {
     const {title, image, id, readyInMinutes} = item
         return(
                
-                <div className="recipe-card">
+                <div className={RecipeCSS['recipe-card']}>
                     <img src={image} alt={title} />
-                    <div className="recipe-info">
-                    <Link to={`/recipe/${id}`} className="recipe-title">
+                    <div className={RecipeCSS['recipe-info']}>
+                    <Link to={`/recipe/${id}`} className={RecipeCSS['recipe-title']}>
                         <h2>{title}</h2>
                     </Link>
-                        <p className="recipe-time"><span><i className="bi bi-clock"></i></span> {readyInMinutes} min</p>
-                    <Link to={`/recipe/${id}`} className="show-details">
+                        <p className={RecipeCSS['recipe-time']}><span><i className="bi bi-clock"></i></span> {readyInMinutes} min</p>
+                    <Link to={`/recipe/${id}`} className={RecipeCSS['show-details']}>
                         <span>Details</span>
-                        <span><i class="bi bi-caret-right"></i></span>
+                        <span><i className="bi bi-caret-right"></i></span>
                     </Link>
                     </div>
                 </div>
