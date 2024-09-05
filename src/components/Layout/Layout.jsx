ba60c7acf45e476dfaaa99b385be6da99c385c0a
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-05 12:02:39 +0200

    feat(Layout): added dynamic padding adjustment based on navbar height
    - Introduced useState to manage navbar height dynamically.
    - Implemented useEffect to calculate and update the padding-top of the container based on the navbar's height.
    - Added a 30px offset.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react";
+
 
 export default function Layout({children}){
+    const [navbarHeight, setNavbarHeight] = useState(0);
+
+  useEffect(() => {
+    const navbar = document.querySelector('.navbar');
+    if (navbar) {
+      setNavbarHeight(navbar.offsetHeight + 30);
+    }
+  }, []);
+
     return (
-    <div className="container">
+    <div className="container" style={{ paddingTop: navbarHeight }}>
         {children}
     </div>
     )
