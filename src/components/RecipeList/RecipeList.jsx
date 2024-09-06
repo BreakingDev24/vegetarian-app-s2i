2699483481cd18896bd348cd4e6fd0f0a50d049e
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-06 20:42:15 +0200

    fix(RecipeList): added conditional rendering for empty recipe list
    - Added condition to handle empty or undefined recipe lists in RecipeList component.
    - Displays a message when no recipes are found, improving UX in cases with no data.

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -2,9 +2,14 @@ import Recipe from "../Recipe/Recipe";
 
 export default function RecipeList(props){
     const {items, title} = props
+
+    if (!items || items.length === 0) {
+        return <p>Nessuna ricetta trovata.</p>;
+    }
     return(
         <>
             <section>
+
                 <div className="title-container">
                     <h2>{title}</h2>
                 </div>
