61da8d09e6d85d969f292eebb9c1980032572795
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-06 11:01:37 +0200

    fix(Layout): adjust navbar height calculation on window load
    - Added function to update the navbar height dynamically on window load to ensure correct spacing.
    - Adjusted the layout padding to reflect the updated navbar height using paddingTop for better layout rendering.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,14 +5,20 @@ export default function Layout({children}){
     const [navbarHeight, setNavbarHeight] = useState(0);
 
   useEffect(() => {
-    const navbar = document.querySelector('.navbar');
-    if (navbar) {
-      setNavbarHeight(navbar.offsetHeight + 30);
+    const updateNavbarHeight = () =>{
+
+      const navbar = document.querySelector('.navbar');
+      if (navbar) {
+      console.log(navbar.offsetHeight + 30)
+        setNavbarHeight(navbar.offsetHeight + 30);
+      }
     }
+
+    window.onload = updateNavbarHeight
   }, []);
 
     return (
-    <div className="container" style={{ paddingTop: navbarHeight }}>
+    <div className="container" style={{ paddingTop: `${navbarHeight}px` }}>
         {children}
     </div>
     )
