cfd8ae65113d5eac133c4d9d0f8861c71f11caa9
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-08 14:25:34 +0200

    feat(context): add RecipeContext for managing recipe data
    - Created RecipeContext to manage global state for recipes, loading, and error states.
    - Implemented RecipeProvider component to provide context values to the component tree.
    - Integrated useFetch hook to fetch recipes based on the query.
    - Added functionality to update query and handle API responses centrally through context."

---
+++ src/components/context/DataRecipeProvider.jsx
@@ -0,0 +1,19 @@
+import { createContext, useEffect, useState, useCallback } from "react";
+import { useFetch } from "../hook/useFetch";
+
+export const RecipeContext = createContext()
+
+export function RecipeProvider ({children}) {
+    const [query, setQuery] = useState("");
+
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&diet=vegetarian&addRecipeInformation=true&apiKey=${apiKey}`
+
+    const {data: recipes, loading, error} = useFetch(url)
+
+    return(
+        <RecipeContext.Provider value={{recipes, loading, error, setQuery}}>
+            {children}
+        </RecipeContext.Provider>
+    )
+}
