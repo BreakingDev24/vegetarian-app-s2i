855c63b0f00b5fcd7d7be45f98c076973c956793
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-08 18:34:04 +0200

    refactor: Added loading spinner for better UX during data fetch.

--- src/pages/RecipeDetails/RecipeDetails.jsx
+++ src/pages/RecipeDetails/RecipeDetails.jsx
@@ -1,50 +1,61 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
-import DetailCSS from './RecipeDetails.module.css'
+import Spinner from "../../components/Spinner/Spinner";
+import { useParams } from "react-router-dom";
+import DetailCSS from "./RecipeDetails.module.css";
 import { useFetch } from "../../components/hook/useFetch";
 
-export default function RecipeDetails(){
-    const {id} = useParams();
-    const apiKey = import.meta.env.VITE_API_KEY;
+export default function RecipeDetails() {
+  const { id } = useParams();
+  const apiKey = import.meta.env.VITE_API_KEY;
 
-    const {data: recipe, loading, error} = useFetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`)
+  const {
+    data: recipe,
+    loading,
+    error,
+  } = useFetch(
+    `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
+  );
 
-    if(loading) return <p>Loading...</p>
-    if (!recipe) return <p>Loading...</p>;
-    const {title, summary, image, extendedIngredients, analyzedInstructions} = recipe
+  if (loading) return <Spinner />;
+  if (!recipe) return <Spinner />;
+  const { title, summary, image, extendedIngredients, analyzedInstructions } =
+    recipe;
 
-    return(
-        <>
-           
-            <main>
-                <h2 className={DetailCSS['recipe-title']}>{title}</h2>
-                <p dangerouslySetInnerHTML={{__html: summary}} className={DetailCSS['recipe-description']}></p>
-                <div className={DetailCSS['details-container']}>
-                    <div className={DetailCSS['recipe-img-container']}>
-                        <img className={DetailCSS['recipe-img']} src={image} alt={title} />
-                    </div>
-                    <div className={DetailCSS['ingredients-container']}>
-                        <h3>Ingredients</h3>
-                        <ul className={DetailCSS['ingredients-list']}>
-                            {
-                                extendedIngredients.map(({original}, index) => {
-                                    return <li key={index} className={DetailCSS.ingredient}>{original}</li>
-                                    
-                                })
-                            }
-                        </ul>
-                    </div>
-                </div>
+  return (
+    <>
+      <main>
+        <h2 className={DetailCSS["recipe-title"]}>{title}</h2>
+        <p
+          dangerouslySetInnerHTML={{ __html: summary }}
+          className={DetailCSS["recipe-description"]}
+        ></p>
+        <div className={DetailCSS["details-container"]}>
+          <div className={DetailCSS["recipe-img-container"]}>
+            <img className={DetailCSS["recipe-img"]} src={image} alt={title} />
+          </div>
+          <div className={DetailCSS["ingredients-container"]}>
+            <h3>Ingredients</h3>
+            <ul className={DetailCSS["ingredients-list"]}>
+              {extendedIngredients.map(({ original }, index) => {
+                return (
+                  <li key={index} className={DetailCSS.ingredient}>
+                    {original}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
 
-                <div className={DetailCSS['istructions-container']}>
-                    <h3>Instructions</h3>
-                    <ol className={DetailCSS['istructions-list']}>
-                    {analyzedInstructions[0]?.steps.map(({ step }, index) => (
-                <li key={index}>{step}</li>
+        <div className={DetailCSS["istructions-container"]}>
+          <h3>Instructions</h3>
+          <ol className={DetailCSS["istructions-list"]}>
+            {analyzedInstructions[0]?.steps.map(({ step }, index) => (
+              <li key={index}>{step}</li>
             ))}
-                    </ol>
-                </div>
-            </main>
+          </ol>
+        </div>
+      </main>
     </>
-    )
+  );
 }
--- src/pages/SearchResults/SearchResults.jsx
+++ src/pages/SearchResults/SearchResults.jsx
@@ -2,16 +2,17 @@ import { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import RecipeList from "../../components/RecipeList/RecipeList";
 import { RecipeContext } from "../../components/context/DataRecipeProvider";
+import Spinner from "../../components/Spinner/Spinner";
 
-export default function SearchResults(){
-    const {recipes, loading, error} = useContext(RecipeContext)
-    const {query} = useParams()
+export default function SearchResults() {
+  const { recipes, loading, error } = useContext(RecipeContext);
+  const { query } = useParams();
 
-    if (loading) return <p>Loading...</p>
+  if (loading) return <Spinner />;
 
-    return (
-        <main>
-            <RecipeList items={recipes} title={`Recipes: ${query}`}/>
-        </main>
-    )
+  return (
+    <main>
+      <RecipeList items={recipes} title={`Recipes: ${query}`} />
+    </main>
+  );
 }
