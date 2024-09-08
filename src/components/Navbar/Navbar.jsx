1a3bb0df82beda6612b0f0f842252df4976f5644
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-08 21:30:18 +0200

    fix: reset query when click on home NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,32 @@
-import { NavLink } from "react-router-dom"
-import logo from '../../assets/img/logo.png'
-import style from './Navbar.module.css'
-export default function Navbar(){
-    return(
-        <>
-        <header>
-            <nav className={`navbar fixed-top ${style.nav} `}> 
-                <div className={style["logo-container"]}>
-                    <img src={logo} alt="logo" />
-                </div>
-                
-                <span>
-                    <NavLink to="/" className={({ isActive }) => isActive ? style.activeLink : style.inactiveLink}><i className="bi bi-house-door-fill">Home</i></NavLink>
-                </span>
-                
-            </nav>
+import { useContext } from "react";
+import { RecipeContext } from "../context/DataRecipeProvider";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/img/logo.png";
+import style from "./Navbar.module.css";
+export default function Navbar() {
+  const { resetQuery } = useContext(RecipeContext);
+
+  return (
+    <>
+      <header>
+        <nav className={`navbar fixed-top ${style.nav} `}>
+          <div className={style["logo-container"]}>
+            <img src={logo} alt="logo" />
+          </div>
+
+          <span>
+            <NavLink
+              to="/"
+              onClick={resetQuery}
+              className={({ isActive }) =>
+                isActive ? style.activeLink : style.inactiveLink
+              }
+            >
+              <i className="bi bi-house-door-fill">Home</i>
+            </NavLink>
+          </span>
+        </nav>
       </header>
-        </>
-    )
+    </>
+  );
 }
--- src/components/context/DataRecipeProvider.jsx
+++ src/components/context/DataRecipeProvider.jsx
@@ -1,19 +1,25 @@
 import { createContext, useEffect, useState, useCallback } from "react";
 import { useFetch } from "../hook/useFetch";
 
-export const RecipeContext = createContext()
+export const RecipeContext = createContext();
 
-export function RecipeProvider ({children}) {
-    const [query, setQuery] = useState("");
+export function RecipeProvider({ children }) {
+  const [query, setQuery] = useState("");
 
-    const apiKey = import.meta.env.VITE_API_KEY;
-    const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&diet=vegetarian&addRecipeInformation=true&apiKey=${apiKey}`
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&diet=vegetarian&addRecipeInformation=true&apiKey=${apiKey}`;
 
-    const {data: recipes, loading, error} = useFetch(url)
+  const { data: recipes, loading, error } = useFetch(url);
 
-    return(
-        <RecipeContext.Provider value={{recipes, loading, error, setQuery}}>
-            {children}
-        </RecipeContext.Provider>
-    )
+  const resetQuery = () => {
+    setQuery("");
+  };
+
+  return (
+    <RecipeContext.Provider
+      value={{ recipes, loading, error, setQuery, resetQuery }}
+    >
+      {children}
+    </RecipeContext.Provider>
+  );
 }
