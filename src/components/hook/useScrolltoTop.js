c78288fce516e388bb24c0a5e1952501fc1f8541
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-11 14:28:05 +0200

    fix: fixed hook

--- src/components/hook/useScrolltoTop.js
+++ src/components/hook/useScrolltoTop.js
@@ -1,7 +1,9 @@
 import { useLayoutEffect, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-useLayoutEffect(() => {
+export default function useScrollTop() {
   const { pathname } = useLocation();
-  document.documentElement.scrollTo({ top: 0, left: 0, behavior: "instant" });
-}, [pathname]);
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [pathname]);
+}
